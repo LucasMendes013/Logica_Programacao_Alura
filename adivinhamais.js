37d57e37660c4df2e58011a59cb3502a781f5ffb
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-24 14:53:19 -0300

    commit js

---
+++ adivinhamais.js
@@ -0,0 +1,16 @@
+var segredo = Math.round(Math.random() * 10)
+var input = document.querySelector("input")
+input.focus()
+
+function verifica(){
+    if (input.value == segredo) {
+    alert("Você acertou")
+    }else {
+        alert("Você errou")
+    }
+    input.value = ""
+    input.focus()
+}   
+
+var button = document.querySelector("button")
+button.onclick = verifica
